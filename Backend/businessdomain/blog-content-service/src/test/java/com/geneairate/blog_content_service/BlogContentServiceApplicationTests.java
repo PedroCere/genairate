d2bef9b413048e9b293ffba9e7b1ea70e949c3fa
DMateo <93503252+Teobac123@users.noreply.github.com>
2025-05-01 16:55:13 -0300

    Add Backend

---
+++ Backend/businessdomain/blog-content-service/src/test/java/com/geneairate/blog_content_service/BlogContentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geneairate.blog_content_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogContentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
